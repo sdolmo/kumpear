22d9a64a28967456b2682dd525cfec69a2059289
sdolmo <sylviad1221@gmail.com>
2015-10-04 14:16:19 -0400

    include css

--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,7 @@ ActiveRecord::Schema.define(version: 20151004004322) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.text     "first_name"
-    t.text     "last_name"
+    t.text     "name"
     t.text     "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
